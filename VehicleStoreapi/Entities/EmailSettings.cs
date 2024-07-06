f9883c4dd7d997e12b25e93a60d1a03e985fb5c4
Teteu <you@example.com>
2024-07-06 15:54:00 -0300

    initial commit, autenticacao de usuario e migrations concluidas

---
+++ VehicleStoreapi/Entities/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace VehicleStoreapi.Database;
+
+public class EmailSettings
+{
+    public string SMTPServer { get; set; }
+    public int SMTPPort { get; set; }
+    public string SMTPUsername { get; set; }
+    public string SMTPPassword { get; set; }
+    public string SenderEmail { get; set; }
+    public string SenderName { get; set; }
+}
---
+++ VehicleStoreapi/extensions/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using VehicleStoreapi.Database;
+
+namespace VehicleStoreapi.extensions;
+
+public class EmailSender : IEmailSender
+{
+    private readonly EmailSettings _emailSettings;
+
+    public EmailSender(IOptions<EmailSettings> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;
+    }
+
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        var client = new SmtpClient(_emailSettings.SMTPServer, _emailSettings.SMTPPort)
+        {
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential(_emailSettings.SMTPUsername, _emailSettings.SMTPPassword),
+            EnableSsl = true,
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(_emailSettings.SenderEmail, _emailSettings.SenderName),
+            Subject = subject,
+            Body = htmlMessage,
+            IsBodyHtml = true,
+        };
+
+        mailMessage.To.Add(email);
+
+        return client.SendMailAsync(mailMessage);
+    }
+}
